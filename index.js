cd1d60760566ca303f899fec1ced76ca7c9543bd
Seldo <github@seldo.com>
2014-02-19 15:17:27 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+exports.register = function (plugin, options, next) {
+
+  console.log("mysql plugin registered")
+
+  if(!options.host) {
+    throw new Error("DB host not defined in config")
+  }
+
+  var pool = mysql.createPool(options);
+
+  console.log(pool)
+
+  plugin.expose('pool', pool);
+  next();
+
+}
