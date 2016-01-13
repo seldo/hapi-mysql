445324b109e49a5c3756d4d2bcc769d19774834f
Lucas Burg <lucasburgmota@gmail.com>
2016-01-13 11:06:35 -0200

    Let's go  create an example to show how the plugin is register

--- example/server.js
+++ example/server.js
@@ -11,6 +11,20 @@ const server = new Hapi.Server();
 server.connection({port: 3000});
 
 
+server.register({
+    require: require('./lib/index.js'),
+    option: {
+        
+    }
+}, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.info('The plugins were load!');
+    }
+});
+
+
 server.start((err) => {
    if (err) {
        console.error(err);
