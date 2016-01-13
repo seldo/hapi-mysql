4f78d07fa8f260e97ebed32958d342517caa2c5c
Lucas Burg <lucasburgmota@gmail.com>
2016-01-13 14:21:26 -0200

    Suport a multiple connections and mora extends config mysql options

--- example/server.js
+++ example/server.js
@@ -13,23 +13,62 @@ server.connection({port: 3000});
 
 server.register({
     register: require('../lib/index.js'),
-    option: {
-        configPlugin: {
-            connType: 'pool' //If undefined the plugin use a simple connection, not pool.
+    options: [
+        {
+            config: {
+                connectName: 'teste',
+                connectType: 'pool'       
+            },
+            connection: {
+                connectionLimit: 10,
+                host: 'localhost',
+                database: 'teste',
+                user: 'root',
+                password: '96342292' 
+            }
         },
-        configConnection: {
-            host: 'localhost',
-            database: 'teste',
-            user: 'teste',
-            password: '96342292'
-        } 
-    }
+        {
+            config: {
+                connectName: 'teste2',
+                connectType: ''       
+            },
+            connection: {
+                host: 'localhost',
+                database: 'teste',
+                user: 'root',
+                password: '96342292'    
+            }
+        }              
+    ] 
 }, (err) => {
     if (err) {
         console.error(err);
     } else {
         console.info('The plugins were load!');
     }
+});
+
+
+server.plugins['hapi-mysql'].teste.getConnection((err, conn) => {
+    
+    conn.query('select * from usuario', (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rows);
+        }
+        
+        conn.release();
+    });
+    
+});
+
+server.plugins['hapi-mysql'].teste2.query('select * from usuario', (err, rows, fields) => {
+   if (err) {
+       console.log(err);
+   } else {
+       console.log(rows);
+   }
 });
 
 
